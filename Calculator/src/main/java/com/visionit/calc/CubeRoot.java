986aaa387f6bf1342d2bd30571fb4cd1f350a97b
Bhushan4192 <chincholkarbhushan@gmail.com>
2020-06-24 19:39:51 +0530

    Added cuberoot class for a test push

---
+++ Calculator/src/main/java/com/visionit/calc/CubeRoot.java
@@ -0,0 +1,14 @@
+package com.visionit.calc;
+
+public class CubeRoot {
+
+	public static void main(String[] args) {
+
+		double x = 27;
+		double y = 81;
+		System.out.println(Math.cbrt(x));
+		System.out.println(Math.cbrt(y));
+
+	}
+
+}
